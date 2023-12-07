d61ebcb1707c0c43903bb0446028d3807f51b839
Arne Brasseur <arne@arnebrasseur.net>
2023-12-07 07:42:04 +0100

    sync

--- src/mad_sounds/sessions/aarschot_2023_12_04_overtone_next.clj
+++ src/mad_sounds/sessions/aarschot_2023_12_04_overtone_next.clj
@@ -1,8 +1,8 @@
 (ns mad-sounds.sessions.aarschot-2023-12-04-overtone-next
   (:require
+   [casa.squid.jack :as jack]
    [overtone.live :refer :all]
-   [vibeflow.util :as u]
-   [casa.squid.jack :as jack]))
+   [vibeflow.util :as u]))
 
 (jack/connect!
  (u/overtone-conns))
@@ -12,3 +12,49 @@
 (fm-demo)
 
 (stop)
+
+(set! *warn-on-reflection* true)
+
+(definst bell [note {:default 60 :min 0 :max 120 :step 1}
+               mod-interval {:default 0 :min -12 :max 24 :step 1}
+               attack {:default 0.01 :min 0 :max 1 :step 0.01}
+               decay  {:default 0.3 :min 0 :max 2 :step 0.01}
+               sustain  {:default 1 :min 0 :max 2 :step 0.01}
+               release {:default 0.5 :min 0 :max 2 :step 0.01}
+               depth {:default 1 :min 1 :max 10 :step 0.1}
+               gate {:default 1 :min 0 :max 1 :step 1}
+               ]
+  (let [freq    (midicps note)
+        modfreq (midicps (+ note mod-interval))
+        mod     (sin-osc :freq modfreq)]
+    (* (env-gen (adsr attack decay sustain release)
+                :gate gate
+                :action FREE)
+       (sin-osc :freq freq :phase (* mod depth)))))
+
+
+(definst bell [note {:default 60 :min 0 :max 120 :step 1}
+               mod-interval {:default 0 :min -12 :max 24 :step 1}
+               attack {:default 0.01 :min 0 :max 1 :step 0.01}
+               decay  {:default 0.5 :min 0 :max 2 :step 0.01}
+               sustain  {:default 0 :min 0 :max 2 :step 0.01}
+               release {:default 0.5 :min 0 :max 2 :step 0.01}
+               depth {:default 1 :min 1 :max 10 :step 0.1}
+               gate {:default 1 :min 0 :max 1 :step 1}
+               ]
+  (let [freq    (midicps note)
+        modfreq (midicps (+ note mod-interval))
+        mod     (pluck :in (white-noise) :delaytime (/ 1 modfreq))]
+    (* (env-gen (adsr attack decay sustain release)
+                :gate gate
+                :action FREE)
+       (sin-osc :freq freq :phase (* mod depth))
+       (sin-osc :freq (* 1.002 freq)
+                :phase (* (env-gen (adsr 0.01 0.3 1 0.1))
+                          mod
+                          depth
+                          )))))
+
+(u/midi-preview :bell #'bell)
+(meta @#'sin-osc)
+(boot-external-server)
--- src/vibeflow/util.clj
+++ src/vibeflow/util.clj
@@ -1,8 +1,7 @@
 (ns vibeflow.util
   (:require
+   [overtone.core :as o]
    [casa.squid.jack :as jack]))
-
-(require '[casa.squid.jack :as jack])
 
 (defn overtone-ports []
   (filter #(re-find #"Overtone|SuperCollider" %)
@@ -13,4 +12,29 @@
        (overtone-ports)
        (jack/ports @jack/default-client #{:audio :physical :in})))
 
-(jack/connect (overtone-conns))
+(defn midi-preview [synth-key synth]
+  (let [voices (atom {})]
+    (o/on-event [:midi :note-on]
+                (fn [{:keys [note]}]
+                  (swap! voices assoc note (synth :note note)))
+                [synth-key :on])
+    (o/on-event [:midi :note-off]
+                (fn [{:keys [note]}]
+                  (o/ctl (get @voices note) :gate 0)
+                  (swap! voices dissoc note))
+                [synth-key :off]))
+  (let [midi-state (atom {})
+        mapping    (into {} (map-indexed (fn [idx p]
+                                           [(+ 21 idx) p])) (:params synth))]
+    (prn mapping)
+    (o/on-event [:midi :control-change]
+                (fn [{:keys [data1 data2]}]
+                  (when-let [{:keys [name default min max step value]}
+                             (get mapping data1)]
+                    (prn name )
+                    (let [v (/ data2 127)
+                          v (+ min (* (- max min) v)) ;; 0-127 -> min-max
+                          v (* step (Math/round (double (/ v step))))] ;; round to nearest step
+                      (reset! value v)
+                      (o/ctl synth (keyword name) v))))
+                [synth-key :ctl])))
