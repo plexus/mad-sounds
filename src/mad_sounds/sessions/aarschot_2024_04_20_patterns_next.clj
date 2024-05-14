ee45d551ca612889ce6c544a88d4b8d51e70dd4e
Arne Brasseur <arne@arnebrasseur.net>
2024-05-14 08:45:06 +0200

    sync

--- src/mad_sounds/sessions/aarschot_2024_04_20_patterns_next.clj
+++ src/mad_sounds/sessions/aarschot_2024_04_20_patterns_next.clj
@@ -4,11 +4,38 @@
 
 (p/pbind {:note [1 2 3 4]
           :dur [1/2 1 1/2]
-          :mtranspose ^{:dur [4 2 4]} [0 5 7]
-          })
+          :mtranspose ^{:dur [4 2 4]} [0 5 7]})
+;; =>
+({:note 1, :dur 1/2, :mtranspose 0}
+ {:note 2, :dur 1, :mtranspose 0}
+ {:note 3, :dur 1/2, :mtranspose 0}
+ {:note 4, :dur 1/2, :mtranspose 0}
+ {:note 1, :dur 1, :mtranspose 0}
+ {:note 2, :dur 1/2, :mtranspose 0}
+ {:note 3, :dur 1/2, :mtranspose 5}
+ {:note 4, :dur 1, :mtranspose 5}
+ {:note 1, :dur 1/2, :mtranspose 5}
+ {:note 2, :dur 1/2, :mtranspose 7})
 
 (p/pnext )
 
 (map meta
      (take-while identity
                  (iterate p/pnext ^{:dur [4 2 4]} [0 5 7])))
+
+
+(take 4
+      (p/pbind
+       {:note (p/pwhite 0 12)
+        :dur (p/pchoose [1 1/2 1/4])
+        :root :c}))
+
+(pbind
+ {:note (pwhite 0 12)
+  :dur (pchoose [1 1/2 1/4])
+  :root :c})
+;;=>
+({:note 1, :dur 1, :root :c}
+ {:note 11, :dur 1/2, :root :c}
+ {:note 11, :dur 1/4, :root :c}
+ {:note 10, :dur 1, :root :c})
---
+++ src/mad_sounds/sessions/eurostar_2024_05_08_events_patterns_docs.clj
@@ -0,0 +1,88 @@
+(ns mad-sounds.sessions.eurostar-2024-05-08-events-patterns-docs
+  (:use overtone.live))
+
+(now)
+
+(event :my-event :foo "asdf" :bar 200.0)
+add-watch
+
+(on-event :my-event println ::print)
+(do
+  (on-event :special-moment
+            (fn [_]
+              (println "special moment")
+              :overtone/remove-handler)
+            ::k)
+
+  ;; Fire the :special-moment event in 3 and 4 seconds
+  (after-delay 3000 #(event :special-moment))
+  (after-delay 4000 #(event :special-moment))
+
+  )
+
+(event-debug-on)
+
+(defsynth foo []
+  (sin-osc))
+
+(midi-connected-devices)
+
+
+(definst example [freq 265 gate 1 amp 1]
+  (* (env-gen (adsr) :gate gate :action FREE)
+     amp
+     your-sound-source))
+
+(gloria)
+(eget {:instrument gloria
+       :degree :iii
+       :mode :minor
+       :octave 5
+       :root 0}
+      :midinote)
+(stop)
+(gloria (midi->hz 64))
+
+(def m (metronome 90))
+
+(do
+  (event :note {:instrument gloria
+                :degree 5
+                :bpm 10
+                })
+  )
+(stop)
+
+(let [clock (metronome 120)]
+  (dotimes [i 3]
+    (event :note {:instrument gloria
+                  :clock clock
+                  :beat (+ i (clock))
+                  :dur 1/2})))
+
+(pplay ::x
+       [{:instrument gloria
+         :note       :c4
+         :dur        1}
+        {:instrument gloria
+         :note       :d4
+         :dur        1/2}
+        {:instrument gloria
+         :note       :e4
+         :dur        1}])
+
+(gloria)
+(stop)
+(note-info :c4)
+{:match "c",  :pitch-class :C, :interval 0}
+{:match "c4", :pitch-class :C, :interval 0, :octave 4, :midi-note 60}
+
+(eget {:root :c5} :midinote)
+(eget {:degree 1} :scale-notes)
+
+(map #(eget % :freq)
+     [
+      ])
+
+(eget {:midinote 60} :freq)
+;; => 261.6255653005986
