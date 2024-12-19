fa9c371091359b0bae6c6a32d411fa4b49897b97
Arne Brasseur <arne@arnebrasseur.net>
2024-12-19 09:06:24 +0100

    sync

--- src/mad_sounds/sessions/richmond_2024_11.clj
+++ src/mad_sounds/sessions/richmond_2024_11.clj
@@ -1,5 +1,6 @@
 (ns mad-sounds.sessions.richmond-2024-11
-  (:use overtone.live))
+  (:use overtone.live)
+  (:require [overtone.helpers.lib :refer [defunk]]))
 
 (def cent (java.lang.Math/pow 2 1/1200))
 
@@ -31,20 +32,30 @@
         :octave 4})
 
 (demo
-  20
-  (let [freq (lin-lin (latch (white-noise:kr) (impulse 10))
-                      -1 1
-                      200 800)]
-    (pan2
-     (*
-      (env-gen (asr 0.3 1 0.3) )
-      (lpf
-       (+
-        (pulse
-         (* freq cent)
-         (+ 0.5 (/ (var-saw (latch (white-noise:kr) (impulse 20))) 10)))
-        (pulse
-         (/ freq cent)
-         (+ 0.5 (/ (var-saw (latch (white-noise:kr) (impulse 20))) 10))))
-       (* 20 freq #_(env-gen (envelope [0.1 1 0.8 0.1] [0.3 1.5 0.3])))))))
-  )
+ 20
+ (let [freq (lin-lin (latch (white-noise:kr) (impulse 10))
+                     -1 1
+                     200 800)]
+   (pan2
+    (*
+     (env-gen (asr 0.3 1 0.3) )
+     (lpf
+      (+
+       (pulse
+        (* freq cent)
+        (+ 0.5 (/ (var-saw (latch (white-noise:kr) (impulse 20))) 10)))
+       (pulse
+        (/ freq cent)
+        (+ 0.5 (/ (var-saw (latch (white-noise:kr) (impulse 20))) 10))))
+      (* 20 freq #_(env-gen (envelope [0.1 1 0.8 0.1] [0.3 1.5 0.3])))))))
+ )
+
+(defunk my-fn "" [x 1 y 2]
+  (+ x y))
+
+(my-fn)
+;; => 3
+(my-fn 2)
+;; => 4
+(my-fn :y 3)
+;; => 4
